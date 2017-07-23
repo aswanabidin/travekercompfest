8b310530647719954048b16e2d271584702f600d
aswanabidin <aswanabidin@ymail.com>
2017-07-23 23:19:01 +0800

    Update 23 July

--- app/src/main/java/com/example/aswanabidin/traveker/HalamanAddItenerary.java
+++ app/src/main/java/com/example/aswanabidin/traveker/HalamanAddItenerary.java
@@ -3,12 +3,18 @@ package com.example.aswanabidin.traveker;
 import android.app.DatePickerDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
-import android.graphics.drawable.Drawable;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
-import android.support.design.widget.TabLayout;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -17,23 +23,24 @@ import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.Spinner;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.aswanabidin.traveker.Fragments.HomeFragment;
 import com.example.aswanabidin.traveker.Model.Itenerary;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.UUID;
 
 public class HalamanAddItenerary extends AppCompatActivity implements View.OnClickListener {
 
@@ -41,9 +48,17 @@ public class HalamanAddItenerary extends AppCompatActivity implements View.OnCli
     private StorageReference mStorageRef;
     private FirebaseAuth mAuth;
     private ProgressDialog progressDialog;
-    private Button btnsubmit;
+    private Button btnsubmit, lampirfoto;
     private EditText departureDate, date, location, tourplace, title, description;
     int day, month, year;
+    private String userChoosenTask;
+    private File files;
+    private Uri foto = null;
+    private String namefile;
+    private int REQUEST_CAMERA = 0, SELECT_FILE = 1;
+    private ImageView imfoto;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,6 +77,12 @@ public class HalamanAddItenerary extends AppCompatActivity implements View.OnCli
 
         TextView judul = (TextView) toolbar.findViewById(R.id.toolbarTitle);
         judul.setText("Add Story");
+
+        lampirfoto = (Button) findViewById(R.id.btnAddfoto);
+        lampirfoto.setOnClickListener(this);
+        imfoto = (ImageView) findViewById(R.id.foto);
+        imfoto.setOnClickListener(this);
+        imfoto.setAdjustViewBounds(true);
 
         mDatabase = FirebaseDatabase.getInstance();
         mStorageRef = FirebaseStorage.getInstance().getReference();
@@ -104,9 +125,6 @@ public class HalamanAddItenerary extends AppCompatActivity implements View.OnCli
                         } catch (Exception e) {
                             e.printStackTrace();
                         }
-                        if (tanggalpilih < System.currentTimeMillis()) {
-                            Toast.makeText(HalamanAddItenerary.this, "Deadline Harus Melebihi Tanggal Sekarang", Toast.LENGTH_SHORT).show();
-                        } else
                             departureDate.setText(tanggal);
                     }
                 },year, month, day);
@@ -147,7 +165,7 @@ public class HalamanAddItenerary extends AppCompatActivity implements View.OnCli
                             //ambil data dari snapshot
                             Itenerary itenerary = postSnapshot.getValue(Itenerary.class);
                         }
-                        startActivity(new Intent(getApplicationContext(), HalamanItenerary.class));
+                        startActivity(new Intent(getApplicationContext(), HomeFragment.class));
                     }
 
                     @Override
@@ -159,7 +177,6 @@ public class HalamanAddItenerary extends AppCompatActivity implements View.OnCli
 
             }
         });
-
 
     }
 
@@ -180,7 +197,119 @@ public class HalamanAddItenerary extends AppCompatActivity implements View.OnCli
 
     @Override
     public void onClick(View view) {
+        if (view == lampirfoto) {
+            photoBuilder();
+        } else if (view == imfoto) {
 
-
+        }
     }
+
+    @NonNull
+    private Boolean isValid() {
+        if (imfoto.getDrawable() == null) {
+            return false;
+        }
+        return true;
+    }
+
+    private void photoBuilder() {
+        final CharSequence[] options = {"Ambil Foto", "Pilih dari Galeri", "Batal"};
+        AlertDialog.Builder builder = new AlertDialog.Builder(HalamanAddItenerary.this);
+        builder.setTitle("Lampirkan Foto");
+        builder.setItems(options, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int item) {
+                boolean result = Utility.checkPermission(HalamanAddItenerary.this);
+                if (options[item].equals("Ambil Foto")) {
+                    if (result) {
+                        userChoosenTask = "Take Photo";
+                        if (result)
+                            cameraIntent();
+                    }
+                } else if (options[item].equals("Pilih dari Galeri")) {
+                    userChoosenTask = "Choose from Library";
+                    if (result)
+                        galleryIntent();
+                } else if (options[item].equals("Batal")) {
+                    dialog.dismiss();
+                }
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void cameraIntent() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //pilih lokasi penyimpanan file
+        File file = new File(Environment.getExternalStorageDirectory(), UUID.randomUUID().toString() + ".jpg");
+        files = file;
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+        namefile = Uri.fromFile(file).getLastPathSegment();
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    private void galleryIntent() {
+        Intent intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(intent, SELECT_FILE);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            if (requestCode == SELECT_FILE)
+                onSelectFromGalleryResult(data);
+            else if (requestCode == REQUEST_CAMERA)
+                onCaptureImageResult(data);
+        } else {
+            Toast.makeText(HalamanAddItenerary.this, "Foto gagal diambil, silahkan coba lagi", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void onCaptureImageResult(Intent data) {
+        foto = Uri.fromFile(files);
+        Picasso.with(HalamanAddItenerary.this).load(files).resize(imfoto.getWidth(), 500).centerCrop().into(imfoto);
+    }
+
+
+    @SuppressWarnings("deprecation")
+    private void onSelectFromGalleryResult(Intent data) {
+        Bitmap bm = null;
+        if (data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = HalamanAddItenerary.this.getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String filePath = cursor.getString(columnIndex);
+            cursor.close();
+            File file = new File(filePath);
+            foto = Uri.fromFile(file);
+            Picasso.with(HalamanAddItenerary.this).load(file).resize(imfoto.getWidth(), 500).centerCrop().into(imfoto);
+        }
+    }
+
+    public boolean isOnline() {
+        ConnectivityManager cm =
+                (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case Utility.MY_PERMISSIONS_REQUEST_READ_EXTERNAL_STORAGE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    if (userChoosenTask.equals("Ambil Foto"))
+                        cameraIntent();
+                    else if (userChoosenTask.equals("Pilih Dari Galeri"))
+                        galleryIntent();
+                } else {
+                    Toast.makeText(this, "Akses Tidak Diizinkan", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
+
 }
