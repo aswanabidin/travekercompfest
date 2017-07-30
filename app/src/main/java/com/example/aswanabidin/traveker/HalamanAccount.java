a8fc399ebedd7c0ce41da7e957781224a0f40ce3
hafidhfikri <hafidhfikri.rasyid@gmail.com>
2017-07-30 13:45:27 +0700

    implementasi_halaman_profile

--- app/src/main/java/com/example/aswanabidin/traveker/HalamanAccount.java
+++ app/src/main/java/com/example/aswanabidin/traveker/HalamanAccount.java
@@ -1,9 +1,11 @@
 package com.example.aswanabidin.traveker;
 
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.TestLooperManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -12,8 +14,13 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.aswanabidin.traveker.Utils.BottomNavigationViewHelper;
@@ -21,7 +28,9 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
+
 
 /**
  * Created by aswanabidin on 7/25/17.
@@ -41,75 +50,128 @@ public class HalamanAccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_halaman_account);
-        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
-
-        Log.d(TAG, "onCreate: started.");
-
-        setupBottomNavigationView();
-
-        Button btnRegister = (Button) findViewById(R.id.btndaftar) ;
-
-        btnRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                btnRegister(view);
-            }
-        });
-
-        Button btnImport = (Button) findViewById(R.id.btnimport);
-        btnImport.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                btnImport(view);
-            }
-        });
-
-        txtEmail = (EditText) findViewById(R.id.etemaillogin);
-        txtPass = (EditText) findViewById(R.id.etpass);
-        btnLogin = (Button) findViewById(R.id.btnlogin);
-
         auth = FirebaseAuth.getInstance();
+        FirebaseUser isLogin = auth.getCurrentUser();
 
-        progressDialog = new ProgressDialog(this);
+        if(isLogin == null) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_halaman_account);
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+            Log.d(TAG, "onCreate: started.");
 
-                String email = txtEmail.getText().toString().trim();
-                String pass = txtPass.getText().toString().trim();
+            setupBottomNavigationView();
 
-                if (TextUtils.isEmpty(email)){
-                    Toast.makeText(HalamanAccount.this, "Enter email address!", Toast.LENGTH_SHORT).show();
-                    return;
+            Button btnRegister = (Button) findViewById(R.id.btndaftar);
+
+            btnRegister.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    btnRegister(view);
                 }
+            });
 
-                if (TextUtils.isEmpty(pass)){
-                    Toast.makeText(HalamanAccount.this, "Enter password!", Toast.LENGTH_SHORT).show();
-                    return;
+            Button btnImport = (Button) findViewById(R.id.btnimport);
+            btnImport.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    btnImport(view);
                 }
+            });
 
-                auth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressDialog.setMessage("Login..");
-                        progressDialog.show();
-                        if(!task.isSuccessful()){
-                            progressDialog.dismiss();
-                            Toast.makeText(HalamanAccount.this,"Login Failed!",Toast.LENGTH_SHORT).show();
-                        }else{
-                            //pindahin ke halaman main
-                            Intent intent = new Intent(HalamanAccount.this, HalamanProfile.class);
-                            startActivity(intent);
-                        }
+            txtEmail = (EditText) findViewById(R.id.etemaillogin);
+            txtPass = (EditText) findViewById(R.id.etpass);
+            btnLogin = (Button) findViewById(R.id.btnlogin);
+
+
+            progressDialog = new ProgressDialog(this);
+
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    progressDialog.setMessage("Login..");
+                    progressDialog.show();
+
+                    String email = txtEmail.getText().toString().trim();
+                    String pass = txtPass.getText().toString().trim();
+
+                    if (TextUtils.isEmpty(email)) {
+                        Toast.makeText(HalamanAccount.this, "Enter email address!", Toast.LENGTH_SHORT).show();
+                        return;
                     }
-                });
-            }
-        });
 
-        return;
+                    if (TextUtils.isEmpty(pass)) {
+                        Toast.makeText(HalamanAccount.this, "Enter password!", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+
+                    auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+
+                            if (!task.isSuccessful()) {
+                                progressDialog.dismiss();
+                                Toast.makeText(HalamanAccount.this, "Login Failed!", Toast.LENGTH_SHORT).show();
+                            } else {
+                                //pindahin ke halaman main
+                                Intent intent = new Intent(HalamanAccount.this, HalamanProfile.class);
+                                startActivity(intent);
+                            }
+                        }
+                    });
+                }
+            });
+
+            return;
+        }else{
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_halaman_account_login);
+            overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+
+            Log.d(TAG, "onCreate: started.");
+
+            setupBottomNavigationView();
+
+            String[] opt = {"Logout","Setting"};
+
+            //set email
+            TextView email = (TextView) findViewById(R.id.txtEmailProfile);
+            email.setText(isLogin.getEmail());
+
+            //set listView
+            ListView listView = (ListView) findViewById(R.id.listOption2);
+            ArrayAdapter adapter = new ArrayAdapter<String>(this,R.layout.activity_listview,opt);
+
+            listView.setAdapter(adapter);
+
+            progressDialog = new ProgressDialog(this);
+
+            listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+                @Override
+                public void onItemClick(AdapterView<?> parent, View view,
+                                        int position, long id) {
+
+                    int itemPosition = position;
+
+                   if(itemPosition == 0){
+                       //jika menekan logout di posisi ke 0
+                       progressDialog.setMessage("Logout..");
+                       progressDialog.show();
+
+                       auth.signOut();
+
+                       progressDialog.dismiss();
+                       Toast.makeText(HalamanAccount.this, "Logout success!", Toast.LENGTH_SHORT).show();
+
+                       Intent intent = new Intent(HalamanAccount.this, HalamanHome.class);
+                       startActivity(intent);
+                   }else if(itemPosition == 1){
+                       //jika menekan logout di posisi ke 1
+                   }
+                }
+            });
+        }
     }
 
     public void btnRegister(View view) {
